50410fc2ff0419662bc1612c4626c0d977e1df1e
mashuk-dev <mushfiqur@aapbd.com>
2022-02-03 04:47:46 +0600

    Some necessary JSX learning

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function buttonText() {
+    return "Click Me !";
+}
+
 const App = () => {
+
+    {/* binding variables */ }
+
+    const buttonTextUnused = "Click Me";
+
+    {/* defining objects */}
+    const myObj = { text: "Click Me" };
+    {/* can use object as myObj.text */}
+
     return (
-        <div>Hello, world!</div>
+        <div>
+            <label htmlFor="email">Email </label>
+            {/* class defination className */}
+            <input type="text" className="form-control" id="email"></input>
+            {/* inline styles in JSX & binding variables */}
+            <button style={{ backgroundColor: 'red',color: 'white' }}>{ buttonText() }</button>
+        </div>
     )
 }
 
