e12e4e46757f48ed95dca0d11cf0c58719d54a6c
mashuk-dev <mushfiqur@aapbd.com>
2022-02-04 04:17:26 +0600

    Using state, setState and conditional rendering

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    render() {
+
+    constructor(props) {
+        super(props)
+
+        this.state = { latitude: 0}
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (error) => console.log(error)
+            (position) => {
+                this.setState({ latitude: position.coords.latitude,  errorMessage: '' })
+            },
+            (error) => this.setState({ errorMessage: error.message })
         );
-        return (
-            <div>
-                Hello, world!
-            </div>
-        )
+    }
+
+    render() {
+
+        if (this.state.errorMessage && !this.state.latitude) {
+            return <div> Error : {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.latitude) {
+            return <div> Latitude is: {this.state.latitude} </div>
+        }
+        else {
+            return <div>Loading...</div>
+        }
     }
 }
 
