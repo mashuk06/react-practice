544bd0985018aa5157f54b0bf6faabe2f5f02b1b
mashuk-dev <mushfiqur@aapbd.com>
2022-02-04 03:40:18 +0600

    Class based component, Removing functional component

---
+++ src/components/HamisphereDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HamisphereDisplay = () => {
+    return (
+        <div className="display">
+            You are in northern hamisphere
+        </div>
+    )
+}
+
+export default HamisphereDisplay;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SingleComment from "./SingleComment"
-import UserCard from "./UserCard"
-import avatar1 from "./images/image-1.jpg"
-import avatar2 from "./images/image-2.jpg"
-import avatar3 from "./images/image-3.jpg"
 
-const App = () => {
-    return (
-        <div className="ui comments">
-            <UserCard>
-                <SingleComment
-                    author="John"
-                    date="Today at 5:00 PM"
-                    text="Great Job"
-                    avatar={avatar1}
-                />
-            </UserCard>
-            <UserCard>
-                <SingleComment
-                    author="Doe"
-                    date="Today at 6:00 PM"
-                    text="Thanks"
-                    avatar={avatar2}
-                />
-            </UserCard>
-            <UserCard>
-                <SingleComment
-                    author="Cena"
-                    date="Today at 7:00 PM"
-                    text="This is amazing!"
-                    avatar={avatar3}
-                />
-            </UserCard>
-
-        </div>
-    )
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        );
+        return (
+            <div>
+                Hello, world!
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
